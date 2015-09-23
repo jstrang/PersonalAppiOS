5685f80d03a47cdb7e8ad857c76da7a6977508f8
Jake Strang <JRS1201@STL42JRS040318.local>
2015-09-23 13:42:04 -0500

    Beginning of tabbed application

---
+++ Jake Strang/Jake Strang/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Jake Strang
+//
+//  Created by Jake Strang on 9/23/15.
+//  Copyright © 2015 jstrang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
